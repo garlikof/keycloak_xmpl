8b6cea9331e3183c3c67465709c86c743dcbcd93
garlikoff <garlikoff@gmail.com>
2024-01-05 13:57:05 +0300

    work with new keycloak

--- src/main/java/org/garlikoff/controller/SampleController.java
+++ src/main/java/org/garlikoff/controller/SampleController.java
@@ -1,11 +1,8 @@
 package org.garlikoff.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,5 +37,11 @@ public class SampleController {
     public Object getMe() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName();
+    }
+
+    @GetMapping("/me2")
+    public Authentication whoAmI(){
+        SecurityContext context = SecurityContextHolder.getContext();
+        return context.getAuthentication();
     }
 }
