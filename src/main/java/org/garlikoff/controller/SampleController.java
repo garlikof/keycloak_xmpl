d25cb9e3c0e5dde1861000a0a180d5a492fd9474
garlikoff <garlikoff@gmail.com>
2024-01-05 12:39:36 +0300

    init commit. work with old keycloak

---
+++ src/main/java/org/garlikoff/controller/SampleController.java
@@ -0,0 +1,44 @@
+package org.garlikoff.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class SampleController {
+
+    @GetMapping("/anonymous")
+    public String getAnonymousInfo() {
+        return "Anonymous";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String getUserInfo() {
+        return "user info";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getAdminInfo() {
+        return "admin info";
+    }
+
+    @GetMapping("/service")
+    @PreAuthorize("hasRole('SERVICE')")
+    public String getServiceInfo() {
+        return "service info";
+    }
+
+    @GetMapping("/me")
+    public Object getMe() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+}
