f8b5c9564e2aaa36d1e85e6e5b17285385c2c93e
garlikoff <garlikoff@gmail.com>
2024-02-25 20:29:13 +0300

    get user details from token, search in DB

--- src/main/java/org/garlikoff/controller/SampleController.java
+++ src/main/java/org/garlikoff/controller/SampleController.java
@@ -1,9 +1,14 @@
 package org.garlikoff.controller;
 
+import org.garlikoff.data.Menu;
+import org.garlikoff.data.RoleAccess;
+import org.garlikoff.data.repository.RoleAccessRepository;
+import org.garlikoff.user.UserDetails;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -11,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -39,8 +45,21 @@ public class SampleController {
         return "service info";
     }
 
+    @Autowired
+    RoleAccessRepository roleAccessRepository;
     @GetMapping("/me")
     public Object getMe() {
+        UserDetails user = new UserDetails();
+
+        user.getRoles().forEach(role -> {
+            Optional<RoleAccess> rr = roleAccessRepository.findById(role);
+            //System.out.println(rr.stream().map(RoleAccess::getName));
+            //System.out.println(rr.orElseGet( r-> r.getMenu().getName()));
+            Optional<Menu> menu = rr.map(RoleAccess::getMenu);
+            System.out.println(menu.map(Menu::getName));
+        });
+
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         KeycloakPrincipal principal2 = (KeycloakPrincipal)auth.getPrincipal();
         KeycloakSecurityContext session = principal2.getKeycloakSecurityContext();
